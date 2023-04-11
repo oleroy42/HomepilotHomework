668ebf906e34f0b5cfaf2fd61addcd1c661790cb
Olivier Leroy <olivier@predict.cash>
2023-04-11 17:23:44 +0200

    add typescript + a bit of typing

--- frontend/homepilot/src/App.tsx
+++ frontend/homepilot/src/App.tsx
@@ -2,20 +2,17 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [hello, setHello] = useState('loading');
+  const [hello, setHello] = useState<string | null >(null);
 
   useEffect(() => {
-    let mounted = true;
     axios.get('https://localhost:7067/amendments/hello')
       .then(res => {
-          if(mounted) {
             setHello(res.data);
-          }
         }
       )
   }, );
 
-  return (<div>{hello}</div>);
+  return (<div>{!!hello ? hello : 'loading' }</div>);
   
 }
 
